f8aaecef59eeffda454b1fc1f79768a81319b9a0
Mati <mati.schneider@markforged.com>
2022-09-30 14:56:49 -0400

    added restart button

--- helpers/dep_functions.py
+++ helpers/dep_functions.py
@@ -1,9 +1,86 @@
 # Check if applications are installed
+import getpass
 import os
 import wget
 
 
 
+
+org_identifier = 'com.markforged'
+dep_notify_log_path = '/var/tmp/dep_notify.log'
+temp_utilities_path = 'usr/local/bin/dep_notify_with_installers'
+installer_base_string = f'{org_identifier}.DEPNotify-prestarter'
+installer_script_name = f'{installer_base_string}-installer.zsh'
+installer_script_path = f'{temp_utilities_path}/{installer_script_name}'
+
+
+def clean_dock():
+    pass
+
+
+def name_computer():
+    pass
+
+
+
+def install_1password():
+    pass
+
+
+
+def install_chrome():
+    pass
+
+
+def install_google_drive():
+    pass
+
+
+def install_slack():
+    pass
+
+
+def install_zoom():
+    pass
+
+
+def configure_printers():
+    pass
+
+
+def install_duo():
+    pass
+
+
+def install_s1():
+    pass
+
+
+def finish_cleanup():
+    pass
+
+#########################################################################################
+# Task List
+#########################################################################################
+# The policy array must be formatted "Progress Bar text,Functionname". These will be
+# run in the order as they appear below.
+
+policy_list = {f'Cleaning Dock,{clean_dock()}', f'Naming Computer,{name_computer()}',
+               f'Installing 1Password, {install_1password()}',
+               f'Installing Chrome, {install_chrome()}',
+               f'Installing Google Drive, {install_google_drive()}',
+               f'Installing Slack, {install_slack()}',
+               f'Installing Zoom, {install_zoom()}',
+               f'Configuring Printers, {configure_printers()}',
+               f'Installing Duo Health App, {install_duo()}',
+               f'Installing Sentinel One, {install_s1()}',
+               f'Finish Cleanup, {finish_cleanup()}'
+               }
+
+
+
+for policy in policy_list:
+    print(policy)
 # set dep app location
 def set_dep_app_location():
     applications_path = '/Applications/'
@@ -71,9 +148,46 @@ def check_applications():
         install_duo_health()
     if not check_sentinel_one():
         install_sentinel_one()
-    
-    
-    
+
+
+def ask_for_sudo_password():
+    input('Please enter your sudo password: ')
+
+
+def set_user_input_plist():
+    dep_notify_user_input_plist = f"/Users/{getpass.getuser()}/Library/Preferences/menu.nomad.DEPNotifyUserInput.plist"
+    if os.path.exists(dep_notify_user_input_plist):
+        pass
+    else:
+        os.system(f'touch {dep_notify_user_input_plist} | sudo -s {ask_for_sudo_password()}')
 
 
 
+# user notification function
+def notify_user(text, dep_notify_log=dep_notify_log_path):
+    with open(dep_notify_log, 'a+') as log:
+        log.write(f'{text}\n')
+
+    log.close()
+
+
+def clear_dep_logs():
+   with open(dep_notify_log_path, 'w') as log:
+       log.write('')
+       log.close()
+
+def print_dep_logs():
+    with open(dep_notify_log_path, 'r') as log:
+        print(log.read())
+        log.close()
+
+
+def launch_dep_notify():
+    launch_dep_app_command = f'launchctl asuser {os.getuid()} open -a {set_dep_app_location()} --args -path {dep_notify_log_path}'
+    os.system(launch_dep_app_command)
+
+def get_logged_in_user():
+    return getpass.getuser()
+
+def kill_dep_notify():
+    os.system(f'killall -9 "DEPNotify"')
--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 import getpass
+import json
 import os
+import signal
+
+import schedule
+
 import helpers.dep_functions as dep_functions
+import psutil
 
 
-def get_logged_in_user():
-    return getpass.getuser()
 
 
+def restart_button():
+    dep_functions.notify_user('Command: Restart: Restart')
+
+def
 
 
 
 if __name__ == '__main__':
-    dep_functions.install_chrome()
+
+    user = dep_functions.get_logged_in_user().replace(".", " ").capitalize()
+
+    dep_functions.notify_user(f'Command: MainTitle: Hi there {user}!')
+    dep_functions.notify_user('Command: Image: /usr/local/techops/logos/logo.png')
+    dep_functions.notify_user('Command: MainText: Welcome to the TechOps Department!')
+    restart_button()
+
+    dep_functions.notify_user('Status: Installing applications...')
+    dep_functions.launch_dep_notify()
+
+
+
+
+
+
+
+
+
+
+
